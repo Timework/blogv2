188242ee090a6f2445bd3e22e7167938284d1e38
Christopher Martinez <timework.bibo@gmail.com>
2021-02-08 11:33:09 -0600

    first commit

---
+++ db/migrate/20210208170552_create_commets.rb
@@ -0,0 +1,11 @@
+class CreateCommets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :article, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
